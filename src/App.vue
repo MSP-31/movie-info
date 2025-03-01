cd5bedc69cb9ceb1d111e97d5fdd802124db7163
MSP-31 <jhss9747@gmail.com>
2025-03-01 09:52:51 +0900

    vue3 공부

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,117 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <Event :text="text" />
+  <h1>Hello Vue</h1>
+  <div v-for="(movie, i) in data" :key="i" class="item">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title" />
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+      <p>개봉: {{ movie.year }}</p>
+      <p>장르: {{ movie.category }}</p>
+      <button @:click="increseLike(i)">좋아요</button>
+      <span>{{ movie.like }}</span>
+      <p>
+        <button
+          @click="
+            isModal = true;
+            selectedMovie = i;
+          "
+        >
+          상세보기
+        </button>
+      </p>
+    </div>
+  </div>
+  <!-- <Modal /> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./assets/movies"; // 영화 데이터
+import Navbar from "./components/Navbar.vue";
+// import Modal from "./components/Modal.vue";
+import Event from "./components/Event.vue"; // 이벤트 박스
 
 export default {
-  name: 'App',
+  name: "App", // 컴포넌트 명
+  data() {
+    return {
+      isModal: false,
+      data: data,
+      selectedMovie: 0,
+      text: "NEPLIX 강렬한 운명의 드라마, 경기크리처",
+    };
+  },
+  methods: {
+    increseLike(i) {
+      this.data[i].like += 1;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Navbar: Navbar,
+    // Modal: Modal,
+    Event: Event,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body {
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+}
+h1,
+h2,
+h3 {
+  margin-bottom: 1rem;
+}
+p {
+  margin-bottom: 0.5rem;
+}
+button {
+  margin-right: 10px;
+  margin-top: 1rem;
+}
+.item {
+  width: 100%;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+.item figure {
+  width: 30%;
+  margin-right: 1rem;
+}
+.item img {
+  width: 100%;
+}
+.item .info {
+  width: 100%;
+}
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+};
+</script>
+
+<style>
+.navbar {
+  background: #000;
+  padding: 20px;
+  text-align: center;
+}
+
+.navbar a {
+  color: #fff;
+  text-decoration: none;
+  padding: 1em;
+}
+</style>
